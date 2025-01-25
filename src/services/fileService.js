d84f9899824562cae7c51ad76efcee951720b6d6
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-01-25 14:55:25 +0100

    added rqgexp

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -5,14 +5,22 @@ exports.fetchFiles = async (queries) => {
   const results = []
   for (const query of queries) {
     const { directory, pattern } = query
+
     try {
+      const regex = new RegExp(
+        '^' + pattern.replace(/\./g, '\\.').replace(/\*/g, '.*') + '$'
+      )
+
       const files = await fs.readdir(directory)
-      const matchedFiles = files.filter(file => file.includes(pattern))
-      const fileData = await Promise.all(matchedFiles.map(async file => {
-        const filePath = path.join(directory, file)
-        const content = await fs.readFile(filePath)
-        return { fileName: file, content: content.toString('base64') }
-      }))
+      const matchedFiles = files.filter(file => regex.test(file)) // Сравнение через регулярное выражение
+      const fileData = await Promise.all(
+        matchedFiles.map(async file => {
+          const filePath = path.join(directory, file)
+          const content = await fs.readFile(filePath)
+          return { fileName: file, content: content.toString('base64') }
+        })
+      )
+
       results.push({ directory, matchedFiles: fileData })
     } catch (err) {
       results.push({ directory, error: err.message })
