2d167a5e5e492ce88d845e0e071a71a21e32ed95
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-01-28 19:18:41 +0100

    added log redefinition and windows service registration

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 require('dotenv').config()
 const { app } = require('./index')
+const fs = require('fs')
+const path = require('path')
+
 const HOST = process.env.HOST || '127.0.0.1'
+const LOG_DIR = process.env.LOG_DIR || 'logs'
+const logFile = path.join(LOG_DIR, process.env.LOG_FILE)
+
+if (!fs.existsSync(LOG_DIR)) {
+  fs.mkdirSync(LOG_DIR, { recursive: true });
+}
+
+const logStream = fs.createWriteStream(logFile, { flags: 'a' })
+console.log = (...args) => {
+  const message = `${new Date().toISOString()} [INFO]: ${args.join(' ')}\n`;
+  process.stdout.write(message)
+  logStream.write(message)
+}
+
+console.error = (...args) => {
+  const message = `${new Date().toISOString()} [ERROR]: ${args.join(' ')}\n`;
+  process.stderr.write(message)
+  logStream.write(message)
+}
 
 app.listen({ port: process.env.PORT || 7111, host: HOST }, (err, address) => {
   if (err) {
