29516d4d0c93dc73642fa68a539eb25f791b164b
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-12-30 11:51:04 +0100

    register_service_pm2.sh

--- node_client/register_service_pm2.sh
+++ node_client/register_service_pm2.sh
@@ -3,7 +3,7 @@
 # Usage: ./register_service_pm2.sh
 
 cd "$(dirname "$0")"
-pm install pm2 -g
-pm2 start send_files.js --name backuper-client
+pm2 install pm2 -g
+pm2 start send_files_cron.js --name backuper-client
 pm2 save
 pm2 startup
